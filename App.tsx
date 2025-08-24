0b44cfecd10e9b3d113452be30a20d993bdca0ed
lucasroqe <alvim.lucas2@hotmail.com>
2025-08-24 17:06:46 -0300

    ex6

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import Cinco from '@/listas/lista1/ex5'
+import Seis from '@/listas/lista1/ex6'
 
 export default function App() {
   // return <Um/>
   // return <Dois/>
   // return <Tres/>
   // return <Quatro/>
-  return <Cinco/>
+  // return <Cinco/>
+  return <Seis/>
 }
 
---
+++ src/listas/lista1/ex6/index.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import {SafeAreaView, Text, TextInput, TouchableOpacity, StyleSheet, View, Button} from "react-native";
+
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [dados, setDados] = useState<{ email: string; senha: string } | null>(null);
+
+  const handleLogin = () => {
+    setDados({ email, senha });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect={false}
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Digite sua senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
+        maxLength={8}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Logar</Text>
+      </TouchableOpacity>
+
+      {dados && (
+        <View style={styles.resultado}>
+          <Text>Email: {dados.email}</Text>
+          <Text>Senha: {dados.senha}</Text>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    backgroundColor: "#3d3c3cff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+  },
+  button: {
+    backgroundColor: '#c9be2dff',
+    padding: 14,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  resultado: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#e6e6e6",
+    borderRadius: 8,
+  },
+});
