5ceea11e0230ecd790f7df4b3fb738980c43c4ac
lucasroqe <alvim.lucas2@hotmail.com>
2025-08-24 21:53:44 -0300

    l2ex3

--- App.tsx
+++ App.tsx
@@ -2,27 +2,56 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import Onze from "@/listas/lista2/ex1";
-import Um from "@/listas/lista1/ex1"
-import Dois from "@/listas/lista1/ex2"
-import Tres from "@/listas/lista1/ex3"
-import Quatro from "@/listas/lista1/ex4"
+import Um from "@/listas/lista1/ex1";
+import Dois from "@/listas/lista1/ex2";
+import Tres from "@/listas/lista1/ex3";
+import Quatro from "@/listas/lista1/ex4";
 // .... demais exercícios 
-
-import { RootStackParamList } from "@/../types";
+import Icon from "react-native-vector-icons/AntDesign";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Onze">
-        <Drawer.Screen name="Onze" component={Onze}/>
-        <Drawer.Screen name="Um" component={Um}/>
-        <Drawer.Screen name="Dois" component={Dois}/>
-        <Drawer.Screen name="Tres" component={Tres}/>
-        <Drawer.Screen name="Quatro" component={Quatro}/>
-        {/* // .... demais exercícios  */}
+      <Drawer.Navigator initialRouteName="Um">
+        <Drawer.Screen
+          name="Um"
+          component={Um}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Dois"
+          component={Dois}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="user" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Tres"
+          component={Tres}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="setting" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Quatro"
+          component={Quatro}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="star" size={size} color={color} />
+            ),
+          }}
+        />
+        {/* // .... demais exercícios */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- src/listas/lista2/ex2/index.tsx
+++ src/listas/lista2/ex2/index.tsx
@@ -1,109 +1,11 @@
-import logo from "@/../assets/fatec-logo.png";
-import {
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Image,
-} from "react-native";
-import React from "react";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamList } from "@/../types";
+import { View } from 'react-native'
+import React from 'react'
+import Um from '@/listas/lista1/ex1'
 
-type DozeProps = {
-  navigation: NativeStackNavigationProp<RootStackParamList, "Doze">;
-};
-
-export default function Doze({ navigation }: DozeProps) {
+export default function index() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.moldura}>
-        <Image source={logo} style={styles.logo} />
-        <Text style={styles.title}>HOME</Text>
-        <View style={styles.grid}>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Um")}>
-            <Text style={styles.buttonText}>Um</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Dois")}>
-            <Text style={styles.buttonText}>Dois</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Tres")}>
-            <Text style={styles.buttonText}>Tres</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Quatro")}>
-            <Text style={styles.buttonText}>Quatro</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Cinco")}>
-            <Text style={styles.buttonText}>Cinco</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Seis")}>
-            <Text style={styles.buttonText}>Seis</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Sete")}>
-            <Text style={styles.buttonText}>Sete</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Oito")}>
-            <Text style={styles.buttonText}>Oito</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Nove")}>
-            <Text style={styles.buttonText}>Nove</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Dez")}>
-            <Text style={styles.buttonText}>Dez</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+    <View>
+      <Um>
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    justifyContent: "center",
-    backgroundColor: "#d9d9d9",
-  },
-  logo: {
-    width: 140,
-    height: 140,
-    alignSelf: "center",
-    marginBottom: 10,
-    resizeMode: "contain",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  moldura: {
-    alignSelf: "center",
-    maxWidth: 300,
-    width: "100%",
-    borderWidth: 2,
-    borderColor: "#ccc",
-    borderRadius: 10,
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-  grid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    gap: 2,
-  },
-  button: {
-    backgroundColor: "#f4a300",
-    padding: 14,
-    borderRadius: 8,
-    alignItems: "center",
-    marginBottom: 10,
-    width: "48%",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
