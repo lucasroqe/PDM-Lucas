152a0b9eb0c4dd70816464d4bd8b88dcbd0f7c0c
lucasroqe <alvim.lucas2@hotmail.com>
2025-08-24 21:07:55 -0300

    l2ex1

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
-import Onze from '@/listas/lista1/ex11'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Onze from "@/listas/lista2/ex1";
+import Um from "@/listas/lista1/ex1"
+import Dois from "@/listas/lista1/ex2"
+import Tres from "@/listas/lista1/ex3"
+import Quatro from "@/listas/lista1/ex4"
+// .... demais exercícios 
+
+import { RootStackParamList } from "@/../types";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  // return <Um/>
-  // return <Dois/>
-  // return <Tres/>
-  // return <Quatro/>
-  // return <Cinco/>
-  // return <Seis/>
-  // return <Sete/>
-  // return <Oito/>
-  // return <Nove/>
-  return <Onze/>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onze">
+        <Stack.Screen name="Onze" component={Onze}/>
+        <Stack.Screen name="Um" component={Um}/>
+        <Stack.Screen name="Dois" component={Dois}/>
+        <Stack.Screen name="Tres" component={Tres}/>
+        <Stack.Screen name="Quatro" component={Quatro}/>
+        {/* // .... demais exercícios  */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
